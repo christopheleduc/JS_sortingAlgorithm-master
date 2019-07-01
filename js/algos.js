d1d9bf8840dc2316ef6186b347f1f16ac002429c
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-07-01 14:09:54 +0200

    Function distanceFromGrenoble complete!

--- js/algos.js
+++ js/algos.js
@@ -7,11 +7,34 @@ Number.prototype.toRadians = function() {
 // Calculates the distance between Grenoble and the given city
 function distanceFromGrenoble(city)
 {
-  console.log("distanceFromGrenoble - implement me !");
+  console.log("distanceFromGrenoble - complete !");
   var GrenobleLat = 45.166667;
   var GrenobleLong = 5.716667;
 
-  return 1;
+  // mon code
+//console.log("Objet city: " + city);
+//console.log("City Lat: " + city.latitude);
+//console.log("City Long: " + city.longitude);
+
+  let lat1=GrenobleLat;
+  let lat2= parseFloat (city.latitude);
+  let lon1=GrenobleLong;
+  let lon2= parseFloat (city.longitude);
+
+  var R = 6371e3; // metres
+  var φ1 = lat1.toRadians();
+  var φ2 = lat2.toRadians();
+  var Δφ = (lat2-lat1).toRadians();
+  var Δλ = (lon2-lon1).toRadians();
+
+  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  var d = R * c / 1000;
+
+  return d;
 }
 
 // Swap 2 values in array csvData
@@ -21,6 +44,7 @@ function swap(i,j)
 {
   displayBuffer.push(['swap', i, j]); // Do not delete this line (for display)
   console.log("swap - implement me !");
+  
 
 }
 
